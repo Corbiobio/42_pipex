58888e0acd4000cb163839db41c53cd8116c38f9
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-02-06 18:49:50 +0100

    add child func

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 12:08:06 by edarnand          #+#    #+#             */
-/*   Updated: 2025/02/05 19:12:44 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:05:11 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ char	*find_paths_in_env(char **env)
 	paths = NULL;
 	while (env[i] != NULL)
 	{
-		
 		if (ft_strnstr(env[i], "PATH=", 5) != NULL)
 		{
 			paths = env[i] + 5;
@@ -41,6 +40,8 @@ char	*join_cmd_to_path(char *path, char *cmd)
 {
 	char	*tmp;
 
+	if (ft_strchr_index(cmd, '/') != -1)
+		return (cmd);
 	tmp = ft_strjoin(path, "/");
 	if (tmp == NULL)
 		return (NULL);
@@ -49,19 +50,26 @@ char	*join_cmd_to_path(char *path, char *cmd)
 	return (path);
 }
 
+char	**get_cmd(char *cmd_str, char **paths)
+{
+	char	**cmd;
+
+	cmd = ft_split(cmd_str, ' ');
+	if (cmd == NULL)
+	{
+		free_double_pt(paths);
+		exit(EXIT_FAILURE);
+	}
+	return (cmd);
+}
+
 void	execute(char *cmd_str, char **paths)
 {
 	char	**cmd;
 	char	*path;
 	int		i;
 
-	cmd = ft_split(cmd_str, ' ');
-	if (cmd == NULL)
-	{
-		perror("Failed to split");//dprintf ????
-		free_double_pt(paths);
-		exit(EXIT_FAILURE);
-	}
+	cmd = get_cmd(cmd_str, paths);
 	i = 0;
 	while (paths[i] != NULL)
 	{
@@ -71,14 +79,13 @@ void	execute(char *cmd_str, char **paths)
 		if (access(path, X_OK) != -1)
 		{
 			if (execve(path, cmd, NULL) == -1)
-				perror(path);
-			free(path);
+				free(path);
 			break ;
 		}
 		i++;
 		free(path);
 	}
-	//perror("Command not found" + cmd[0]);//dprintf ????
+	perror(cmd[0]);
 	free_double_pt(cmd);
 	free_double_pt(paths);
 	exit(EXIT_FAILURE);
@@ -96,57 +103,109 @@ void	close_all_fd(int fd1, int fd2, int fd3, int fd4)
 		close(fd4);
 }
 
+void	perror_free_exit(char *error, char **to_free)
+{
+	perror(error);
+	free_double_pt(to_free);
+	exit(EXIT_FAILURE);
+}
+
+void	first_child(int *pipe_fd, char **av, char **paths)
+{
+	int	infile;
+
+	infile = open(av[0], O_RDONLY);
+	if (infile == -1)
+	{
+		close_all_fd(infile, pipe_fd[0], pipe_fd[1], -1);
+		perror_free_exit(av[0], paths);
+	}
+	if (dup2(infile, STDIN_FILENO) == -1)
+	{
+		close_all_fd(infile, pipe_fd[0], pipe_fd[1], -1);
+		perror_free_exit("dup2 error", paths);
+	}
+	if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+	{
+		close_all_fd(infile, pipe_fd[0], pipe_fd[1], -1);
+		perror_free_exit("dup2 error", paths);
+	}
+	close_all_fd(infile, pipe_fd[0], pipe_fd[1], -1);
+	execute(av[1], paths);
+}
+
+void	last_child(int *pipe_fd, int i, char **av, char **paths)
+{
+	int	outfile;
+
+	outfile = open(av[i + 1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (outfile == -1)
+	{
+		close_all_fd(outfile, pipe_fd[0], pipe_fd[1], pipe_fd[2]);
+		perror_free_exit(av[0], paths);
+	}
+	if (dup2(pipe_fd[2], STDIN_FILENO) == -1)
+	{
+		close_all_fd(outfile, pipe_fd[0], pipe_fd[1], pipe_fd[2]);
+		perror_free_exit("dup2 error", paths);
+	}
+	if (dup2(outfile, STDOUT_FILENO) == -1)
+	{
+		close_all_fd(outfile, pipe_fd[0], pipe_fd[1], pipe_fd[2]);
+		perror_free_exit("dup2 error", paths);
+	}
+	close_all_fd(outfile, pipe_fd[0], pipe_fd[1], pipe_fd[2]);
+	execute(av[i], paths);
+}
+
+void	mid_child(int *pipe_fd, int i, char **av, char **paths)
+{
+	if (dup2(pipe_fd[2], STDIN_FILENO) == -1)
+	{
+		close_all_fd(pipe_fd[0], pipe_fd[1], pipe_fd[2], -1);
+		perror_free_exit("dup2 error", paths);
+	}
+	if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+	{
+		close_all_fd(pipe_fd[0], pipe_fd[1], pipe_fd[2], -1);
+		perror_free_exit("dup2 error", paths);
+	}
+	close_all_fd(pipe_fd[0], pipe_fd[1], pipe_fd[2], -1);
+	execute(av[i], paths);
+}
+
+void	child_process(int *pipe_fd, int i, char **av, char **paths)
+{
+	if (i == 1)
+		first_child(pipe_fd, av, paths);
+	else if (av[i + 2] == NULL)
+		last_child(pipe_fd, i, av, paths);
+	else
+		mid_child(pipe_fd, i, av, paths);
+}
+
 void	pipex(int ac, char **av, char **paths)
 {
 	int	i;
 	int	pid;
-	int	pipe_fd[2];
-	int	next_read_fd;
+	int	pipe_fd[3];
 
 	i = 1;
 	while (i < ac - 1)
 	{
-		pipe(pipe_fd);//protect
-		pid = fork();//protect
+		if (pipe(pipe_fd) == -1)
+			return ;
+		pid = fork();
 		if (pid < 0)
-			return ;//pas sure
+			return ;
 		if (pid == 0)
-		{
-			if (i == 1)
-			{
-				int	file1 = open(av[0], O_RDONLY); //protect
-				dup2(file1, STDIN_FILENO);//protect
-				dup2(pipe_fd[1], STDOUT_FILENO);//protect
-				close_all_fd(file1, pipe_fd[0], pipe_fd[1], -1);
-				execute(av[1], paths);
-				//protect if exev fail
-			}
-			else if (av[i + 2] == NULL)
-			{
-				int	file2 = open(av[i + 1], O_WRONLY | O_CREAT | O_APPEND); //protect | replace append by trunc
-				dup2(next_read_fd, STDIN_FILENO);//protect
-				dup2(file2, STDOUT_FILENO);//protect
-				close_all_fd(file2, pipe_fd[0], pipe_fd[1], next_read_fd);
-				execute(av[i], paths);
-				//protect if exev fail
-			}
-			else
-			{
-				dup2(next_read_fd, STDIN_FILENO);//protect
-				dup2(pipe_fd[1], STDOUT_FILENO);//protect
-				close_all_fd(pipe_fd[0], pipe_fd[1], next_read_fd, -1);
-				execute(av[i], paths);
-				//protect if exev fail
-			}
-			free_double_pt(paths);
-			exit(EXIT_FAILURE);
-		}
+			child_process(pipe_fd, i, av, paths);
 		else
 		{
 			if (i != 1)
-				close(next_read_fd);
-			next_read_fd = pipe_fd[0];
-			if (i == ac - 2)//if last close all
+				close(pipe_fd[2]);
+			pipe_fd[2] = pipe_fd[0];
+			if (i == ac - 2)
 				close(pipe_fd[0]);
 			close(pipe_fd[1]);
 		}
@@ -157,7 +216,9 @@ void	pipex(int ac, char **av, char **paths)
 int	main(int ac, char **av, char **env)
 {
 	char	**paths;
+	int		status;
 
+	status = 0;
 	if (ac <= 4)
 		ft_putstr_fd("Not enough param", STDERR_FILENO);
 	else
@@ -168,8 +229,9 @@ int	main(int ac, char **av, char **env)
 		if (paths == NULL)
 			exit(EXIT_FAILURE);
 		pipex(ac, av, paths);
-		while (wait(NULL) > 0);
+		while (wait(&status) > 0)
+			;
 		free_double_pt(paths);
 	}
-	return (EXIT_SUCCESS);
+	return (WEXITSTATUS(status));
 }
